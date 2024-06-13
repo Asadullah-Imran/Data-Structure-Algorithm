1722bbc4610a06f31dbcadb24544ea5c46fe83f0
Asadullah Imran <asadullahimran19@gmail.com>
2024-06-13 04:27:32 +0000

    B_Getting_An_A problem solved

--- sorting/codeforce/problem_991_B/B_Getting_An_A.cpp
+++ sorting/codeforce/problem_991_B/B_Getting_An_A.cpp
@@ -9,21 +9,38 @@ void insertionSort(int arr[], int n){
             arr[j+1]=arr[j];
             j--;
         }
-        arr[j]=key;
+        arr[j+1]=key;
     }
 }
 
 double gettingAnA(int arr[],int n,int total){
+    int result=0;
     insertionSort(arr,n);
-    for(int i =0;i<n;i++){
-        std::cout<<arr[i]<<" ";
-    }
+    //  std::cout << std::endl;
+    // for(int i =0;i<n;i++){
+    //     std::cout<<arr[i]<<" ";
+    // }
+    //  std::cout << std::endl;
     double need= (double)(n*4.5)-total;
-    if(need<0){
-        return 0;
+    if(need<=0){
+        return result;
     }else{
-    return need;
+
+        for(int i=0;i<n;i++){
+            // while(need>0){
+            need-=(5-arr[i]);
+            result++;
+            // }
+            if(need<=0){
+                return result;
+            }
+
+        }
+
+
+    // return need;
     }
+    return result;
 }
 
 int main(){
@@ -39,6 +56,7 @@ int main(){
 
     //now getting the result
     double result=gettingAnA(arr,n,total);
-    std::cout<<'\n'<<result<<std::endl;
+    // std::cout<<'\n'<<"Result "<<result<<std::endl;
+    std::cout<<result<<std::endl;
 
 }
