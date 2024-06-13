e5c8efc5f0a46816a1629ebdd7fdea70ce55eed0
Asadullah Imran <asadullahimran19@gmail.com>
2024-06-13 03:56:17 +0000

    insertion sortig

--- sorting/codeforce/problem_991_B/B_Getting_An_A.cpp
+++ sorting/codeforce/problem_991_B/B_Getting_An_A.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
-int gettingAnA(int arr[],int n,int total){
-    double need= (total/n)*4.5;
-    return result;
+
+void insertionSort(int arr[], int n){
+    int key ,j;
+    for(int i=1;i<n;i++){
+        key= arr[i];
+        j=i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j]=key;
+    }
+}
+
+double gettingAnA(int arr[],int n,int total){
+    insertionSort(arr,n);
+    for(int i =0;i<n;i++){
+        std::cout<<arr[i]<<" ";
+    }
+    double need= (double)(n*4.5)-total;
+    if(need<0){
+        return 0;
+    }else{
+    return need;
+    }
 }
 
 int main(){
@@ -16,7 +38,7 @@ int main(){
     }
 
     //now getting the result
-    int result=gettingAnA(arr,n,total):
-    std::cout<<result;
+    double result=gettingAnA(arr,n,total);
+    std::cout<<'\n'<<result<<std::endl;
 
 }
