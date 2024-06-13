4e3ce91c2cd6027405c0b7c44a1b35d45990f4f0
Asadullah Imran <asadullahimran19@gmail.com>
2024-06-13 03:10:16 +0000

    starting getting an A

---
+++ sorting/codeforce/problem_991_B/B_Getting_An_A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+int gettingAnA(int arr[],int n,int total){
+    double need= (total/n)*4.5;
+    return result;
+}
+
+int main(){
+    //Taking input from users
+    int n;
+    std::cin>>n;
+    int arr[n];
+    int total=0;
+    for(int i=0;i<n;i++){
+        std::cin>>arr[i];
+        total+=arr[i];
+    }
+
+    //now getting the result
+    int result=gettingAnA(arr,n,total):
+    std::cout<<result;
+
+}
