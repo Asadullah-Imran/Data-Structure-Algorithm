ac76f8c236f40d6645dfc3e6f32ba5fdc5865d92
Asadullah Imran <asadullahimran19@gmail.com>
2024-06-21 13:52:14 +0000

    verify password working

---
+++ sorting/codeforce/A_Verify_Password/A_Verify_Password.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+bool verifyPassword( std::string str){
+    
+}
+
+int main (){
+    int n;
+    std::cin>>n;
+    std::string str;
+    std::cin>>str;
+    if(verifyPassword(str)){
+        std::cout<<"Yes"<<std::endl;
+    }else{
+        std::cout<<"No"<<std::endl;
+    }
+    // std::cout<<str<<std::endl;
+}
--- sorting/codeforce/problem_1975_A/A_Bazoka_and_Mocha_Array.cpp
+++ sorting/codeforce/problem_1975_A/A_Bazoka_and_Mocha_Array.cpp
@@ -1,37 +1,21 @@
 #include <iostream>
 
-// bool isIncreasing(int arr[], int n, int &index) {
-//     for (int i = 0; i < n - 1; i++) {
-//         if (arr[i] > arr[i + 1]) {
-//             index = i;
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
-bool isIncreasing(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;
-        }
-    }
-    return true;
-}
-bool isOk(int arr[], int n) {
+bool isABazokaAndMocha(int arr[], int n) {
 int breakpoint=0;
     for (int i = 0; i < n - 1; i++) {
         if (arr[i] > arr[i + 1]) {
            breakpoint++;
         }
     }
-     if (breakpoint == 0) {
+    if (breakpoint == 0) {
         // Array is already sorted
         return true;
     }
     if(breakpoint==1){
+        //Array has breakpoints one time.
         return arr[n-1]<=arr[0];
     }
+    //array has many breakpoints
     return false;
     
 }
@@ -46,7 +30,7 @@ int main() {
         for (int i = 0; i < n; i++) {
             std::cin >> arr[i];
         }
-        if (isIncreasing(arr,n)||isOk(arr,n)) {
+        if (isABazokaAndMocha(arr,n)) {
             std::cout <<"Yes"<<std::endl;
         } else {
             std::cout << "No"<<std::endl;
