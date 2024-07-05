cc036a4c571757e3b61c4ade09198fa053816b16
Asadullah Imran <asadullahimran19@gmail.com>
2024-07-05 16:07:32 +0000

    doubly linkedList complete

--- dataStructure/linkedList/doublyLinkedList.cpp
+++ dataStructure/linkedList/doublyLinkedList.cpp
@@ -133,27 +133,70 @@ void deleteAt(int pos)
                 tempNode->prev->next = tempNode->next;
                 tempNode->next->prev = tempNode->prev;
             }
-
             free(tempNode);
-            
-            
         }
 
     }
 }
 
 int search(int element){
+    struct node * tempNode=head;
+    int index=0;
+    while(tempNode!=NULL){
+        if(tempNode->data==element){
+            return index;
+        }else{
+            index++;
+            tempNode=tempNode->next;
+        }
+    }
 
     return -1;
 }
 
 
 int findMax(){
-return -1;
+    if(head==NULL){
+        std::cout<<"This linkedList is empty.";
+        return -1;
+    }
+    struct node * tempNode = head;
+    int index=0,maxIndex=0;
+    int max= head->data;
+
+    while(tempNode!=NULL){
+        if(tempNode->data>max){
+            max=tempNode->data;
+            maxIndex=index;
+        }
+        tempNode=tempNode->next;
+        index++;
+    }
+
+
+return maxIndex;
 }
 
 int findMin(){
-return -1;
+    if(head==NULL){
+        std::cout<<"This linkedList is empty.";
+        return -1;
+    }
+    struct node * tempNode = head;
+    int index=0,minIndex=0;
+    int min= head->data;
+
+    while(tempNode!=NULL){
+        if(tempNode->data<min){
+            min=tempNode->data;
+            minIndex=index;
+        }
+        tempNode=tempNode->next;
+        index++;
+    }
+
+
+return minIndex;
 }
 void displayList()
 {
@@ -166,54 +209,96 @@ void displayList()
 
 int main()
 {
-    // int choice,key,pos;
+    int choice,key,pos;
 
-    // std::cout<<"1.insert first 2.insert last 3.insert at\n";
-    // std::cout<<"4.delete first 5.delete last 6.delete at \n"
-    //        "7.search 8.find max 9.find min 10.exit\n";
-    // while(1){
+    std::cout<<"1.insert first 2.insert last 3.insert at\n";
+    std::cout<<"4.delete first 5.delete last 6.delete at \n"
+           "7.search 8.find max 9.find min 10.exit\n";
+    while(1){
+        
+        std::cout<<std::endl<<"enter choice:";
+        std::cin>>choice;
+        if(choice==1){
+            std::cout<<"Enter element for insert first"<<std::endl;
+            std::cin>>key;
+            insertBeginning(key);
+        }else if(choice==2){
+            std::cout<<"Enter element for insert last"<<std::endl;
+            std::cin>>key;
+            insertLast(key);
+        }else if(choice==3){
+            std::cout<<"Enter element for insert element"<<std::endl;
+            std::cin>>key;
+            std::cout<<"Enter Position for insert at"<<std::endl;
+            std::cin>>pos;
+            insertAt(pos,key);
+        }else if(choice==4){
+            deleteFirst();
+            std::cout<<"First element is deleted"<<std::endl;
+        }else if(choice==5){
+            std::cout<<"Last element is deleted"<<std::endl;
+            deleteLast();
+        }else if(choice==6){
+            std::cout<<"Enter Position for delete at"<<std::endl;
+            std::cin>>pos;
+            deleteAt(pos);
+        }else if(choice==7){
+            std::cout<<"Enter element for Search"<<std::endl;
+            std::cin>>key;
+            int x=search(key);
+            if(x==-1){
+                std::cout<<"Not Matched"<<std::endl;
+            }else{
+                std::cout<<"Matched at index "<<x<<std::endl;
+            }
+        }else if(choice==8){
+            int x=findMax();
+            if(x==-1){
+                std::cout<<"There is no element"<<std::endl;
+            }else{
+                std::cout<<"Max Element is found at index "<<x<<std::endl;
+            }
+        }else if(choice==9){
+            int x=findMin();
+            if(x==-1){
+                std::cout<<"There is no element"<<std::endl;
+            }else{
+                std::cout<<"Min Element is found at index "<<x<<std::endl;
+            }
+        }else if(choice==10){
+            std::cout<<"We are Exiting."<<std::endl;
+            displayList();
+            break;
+        }else{
+            continue;
+        }
+        displayList();
+    }
 
-    //     std::cout<<"enter choice:";
-    //     std::cin>>choice;
-    //     if(choice==1){
-    //             //complete the code
-    //     }else if(choice==2){
-    //         printf("enter element:");
-
-    //     }else if(choice==3){
-
-    //     }else if(choice==4){
-
-    //     }else if(choice==5){
-
-    //     }else if(choice==6){
-
-    //     }else if(choice==7){
-
-    //     }else if(choice==8){
-
-    //     }else if(choice==9){
-
-    //     }else {
-    //         break;
-    //     }
-    //     displayList();
-    // }
 
 
+// insertLast(10);
+// insertLast(20);
+// insertLast(30);
+// insertLast(40);
+// insertBeginning(56);
+// insertAt(5,300);
+// deleteFirst();
+// deleteLast();
+//  deleteAt(1);
 
-insertLast(10);
-insertLast(20);
-insertLast(30);
-insertLast(40);
-insertBeginning(56);
-insertAt(5,300);
-deleteFirst();
-deleteLast();
- deleteAt(1);
+// insertBeginning(1);
+// insertBeginning(100);
 
+// displayList();
 
-displayList();
+// int x= findMin();
+// std::cout<<std::endl;
+// if(x==-1){
+//     std::cout<<"not Matched!"<<std::endl;
+// }else{
+//     std:: cout<<"max at index "<<x<<std::endl;
+// }
 
 
 
